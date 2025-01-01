d166cf48d2633220d2417482d10d679642a6c58d
isAlvarezDev <alvarezisaac871@gmail.com>
2025-01-01 19:19:06 -0400

    The data file, the url were removed all the last part called index.html

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export default [
         languages: ["HTML", "CSS"],
         difficulty: ["1", "NEWBIE"],
         description: "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
-        url: "./learning-paths/qr-code-component-main/index.html",
+        url: "./learning-paths/qr-code-component-main/",
         alt: "qr code project"
     },
     {
@@ -14,7 +14,7 @@ export default [
         languages: ["HTML", "CSS"],
         difficulty: ["1", "NEWBIE"],
         description: "This HTML & CSS-only challenge is a perfect project for beginners getting up to speed with HTML and CSS fundamentals, like HTML structure and the box model.",
-        url: "./learning-paths/blog-card/index.html",
+        url: "./learning-paths/blog-card/",
         alt: "blog preview card project"
     },
     {
@@ -23,7 +23,7 @@ export default [
         languages: ["HTML", "CSS"],
         difficulty: ["1", "NEWBIE"],
         description: "In this small project, you'll build out your social link-sharing profile. You can even personalize it and use it to share all your social profiles!",
-        url: "./learning-paths/social-links-profile/index.html",
+        url: "./learning-paths/social-links-profile/",
         alt: "social links profile project"
     },
     {
@@ -32,7 +32,7 @@ export default [
         languages: ["HTML", "CSS"],
         difficulty: ["1", "NEWBIE"],
         description: "This challenge will help you focus on writing semantic HTML. Ensure you think through what HTML elements are most appropriate for each piece of content.",
-        url: "./learning-paths/recipe-page/index.html",
+        url: "./learning-paths/recipe-page/",
         alt: "recipe page project"
     },
 ]
